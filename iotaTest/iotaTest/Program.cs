1004020cd43dd25c1af283716f209d7288fbc96b
Dominik Lindemann <dominik.lindemann@hotmail.de>
2018-07-21 16:35:50 +0200

    initial checkin

---
+++ iotaTest/iotaTest/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using Tangle.Net.Cryptography;
+using Tangle.Net.Entity;
+using Tangle.Net.Repository;
+using Tangle.Net.Repository.DataTransfer;
+
+namespace iotaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new RestIotaRepository(new RestClient("https://nodes.testnet.iota.org:443"));
+            const string address = "THEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMO";
+
+            Seed seed = new Seed(address);
+            Transfer transfer = new Transfer
+            {
+                Address = new Address(address),
+                Message = TryteString.FromUtf8String("Homomorphic Iota"),
+                ValueToTransfer = 0
+            };
+
+            Bundle bundle = new Bundle();
+            bundle.AddTransfer(transfer);
+
+            int depth = 3;
+            int minWeightMagnitude = 9;
+            try
+            {
+                //var sendTransfer = repository.SendTransfer(seed, bundle, SecurityLevel.Medium, depth, minWeightMagnitude);
+                TransactionHashList transactions = repository.FindTransactionsByAddresses(new List<Address> {new Address(address)});
+
+                var transactionsTrytes = repository.GetTrytes(transactions.Hashes).First();
+                Hash hash = new Hash(transactions.Hashes[0].Value);
+                var transaction = Transaction.FromTrytes(transactionsTrytes, hash);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                //throw;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
