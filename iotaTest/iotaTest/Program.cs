f2af48fa47fd8db3a611f7f51d8d07c27fe60b7c
Dominik Lindemann <dominik.lindemann@hotmail.de>
2018-07-22 12:23:55 +0200

    write new data

--- iotaTest/iotaTest/Program.cs
+++ iotaTest/iotaTest/Program.cs
@@ -18,22 +18,22 @@ namespace iotaTest
             string basePath = Path.Combine(Environment.CurrentDirectory, "..", "..");
             string resultText = File.ReadAllText(Path.Combine(basePath, "result.txt"));
             var repository = new RestIotaRepository(new RestClient("https://nodes.testnet.iota.org:443"));
-            
-            //Message = TryteString.FromUtf8String(resultText),
+
+            string message = resultText;
             //string message = "blubber" + DateTime.Now.Ticks;
-            string message =
-                "1These homomorphic encryption schemes are based on a hard computation problem known as Ring Learning with Errors. Here we will unpack what the Ring part of that title means. Essentially, data in these schemes is represented by polynomials both when it is encrypted (the ciphertext) and when it is unencrypted (the plaintext)." +
-                "2These are almost the normal polynomials that everyone studies at school. Things like There are some differences, the first being that the coefficients are all whole numbers and are the remainder relative to some other whole number (that is ). Say , then this is like a 24 hour clock, where adding 6 hours to 21 takes you to 3. All the coefficients of the polynomials are treated like this." +
-                "3Alternately, we can consider the numbers to range from -11 to 12, allowing us to negate numbers conveniently. Note that this is just a convenience factor - there is no difference between a \"remainder\" of -1 and a remainder of 23 (when dividing by 24)." +
-                "4The second, and trickier, thing that is different about these polynomials is that this idea of using remainders applies not just to the coefficients of the polynomials, but also to the polynomials themselves." +
-                "5We define a special polynomial called the polynomial modulus, and only consider the remainder of polynomials when they have been divided by this polynomial modulus. The specific form of this polynomial modulus in the FV scheme is where for some . For illustration here we will take , so the polynomial is ." +
-                "6Because we are considering remainders with respect to , we only have to consider polynomials with powers from to . Any larger powers will be reduced by division by the polynomial modulus. This can also be understood by considering that , meaning that can be replaced by -1 to reduce the larger powers of into the range 0 to 15.";
+            //string message =
+            //    "1These homomorphic encryption schemes are based on a hard computation problem known as Ring Learning with Errors. Here we will unpack what the Ring part of that title means. Essentially, data in these schemes is represented by polynomials both when it is encrypted (the ciphertext) and when it is unencrypted (the plaintext)." +
+            //    "2These are almost the normal polynomials that everyone studies at school. Things like There are some differences, the first being that the coefficients are all whole numbers and are the remainder relative to some other whole number (that is ). Say , then this is like a 24 hour clock, where adding 6 hours to 21 takes you to 3. All the coefficients of the polynomials are treated like this." +
+            //    "3Alternately, we can consider the numbers to range from -11 to 12, allowing us to negate numbers conveniently. Note that this is just a convenience factor - there is no difference between a \"remainder\" of -1 and a remainder of 23 (when dividing by 24)." +
+            //    "4The second, and trickier, thing that is different about these polynomials is that this idea of using remainders applies not just to the coefficients of the polynomials, but also to the polynomials themselves." +
+            //    "5We define a special polynomial called the polynomial modulus, and only consider the remainder of polynomials when they have been divided by this polynomial modulus. The specific form of this polynomial modulus in the FV scheme is where for some . For illustration here we will take , so the polynomial is ." +
+            //    "6Because we are considering remainders with respect to , we only have to consider polynomials with powers from to . Any larger powers will be reduced by division by the polynomial modulus. This can also be understood by considering that , meaning that can be replaced by -1 to reduce the larger powers of into the range 0 to 15.";
 
             //const string address = "FHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMO";
             //SendToChain(address, repository, message);
 
-            const string address = "IHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMO";
-           // SendToChain(address, repository, message);
+            const string address = "IOTAFFBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINEDMOBILITYHACKATHONTHEBLOCKCHAINE";
+            SendToChain(address, repository, message);
             GetFromChain(address, repository);
             Console.ReadKey();
         }
